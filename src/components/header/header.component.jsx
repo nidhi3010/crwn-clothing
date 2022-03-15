8703ceb7863dd950af7e27f980030b80d3aac996
studiomac-142 <studiomac-142@studiomac-142s-iMac.local>
2022-03-14 17:18:42 -0700

    css

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -13,38 +12,35 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
+
 import './header.styles.scss';
 
 const Header = ({currentUser, hidden}) => (
-    <div className='header'>
-      <Link className='logo-container' to = "/">
+  <HeaderContainer>
+      <LogoContainer to = "/">
           <Logo className='logo' />  
 
-      </Link>
+      </LogoContainer>
 
-      <div className='options'>
-          <Link className='option' to='/shop'>
-              SHOP
-          </Link>
-          <Link className='option' to='/shop'>
-              CONTANT
-          </Link>
+      <OptionsContainer>
+          <OptionLink to='/shop'>SHOP</OptionLink>
+          <OptionLink  to='/shop'>CONTANT</OptionLink>
           {
-              currentUser ?(
-              <div className='option' onClick = {() => auth.signOut()}>
+            currentUser ?(
+              <OptionLink as = 'div' onClick = {() => auth.signOut()}>
                SIGN OUT
-          </div>
+              </OptionLink>
         )  : ( 
-          <Link className='option' to='/signin'>
+          <OptionLink to='/signin'>
               SIGN IN
-         </Link>
+          </OptionLink>
          ) } 
          <CartIcon />
-    </div>
+    </OptionsContainer>
     
        { hidden ? null : <CartDropdown /> }
-       
-    </div> 
+    </HeaderContainer>
 );
 
 const mapStateToProps =  createStructuredSelector ({
